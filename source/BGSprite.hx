0fbcc7ac1230e362644501a25b3bbfc1ec1a15df
T5mpler <realt5mpler@gmail.com>
2022-02-02 19:14:49 -0500

    SHADERRS FUCK YOU

--- source/BGSprite.hx
+++ source/BGSprite.hx
@@ -1,21 +1,7 @@
 package;
 
-import openfl.events.SampleDataEvent;
-import flixel.math.FlxPoint;
-import flixel.tweens.FlxEase;
-import openfl.ui.Keyboard;
-import flixel.util.FlxCollision;
-import flixel.FlxG;
+import flixel.system.FlxAssets.FlxShader;
 import flixel.FlxSprite;
-import flixel.addons.transition.FlxTransitionableState;
-import flixel.graphics.frames.FlxAtlasFrames;
-import flixel.group.FlxGroup.FlxTypedGroup;
-import flixel.group.FlxGroup;
-import flixel.math.FlxMath;
-import flixel.text.FlxText;
-import flixel.tweens.FlxTween;
-import flixel.util.FlxColor;
-import flixel.util.FlxTimer;
 #if desktop
 import Discord.DiscordClient;
 #end
@@ -32,21 +18,27 @@ class BGSprite extends FlxSprite
 		this.name = name;
 		var hasAnimations:Bool = !(animations == null);
 
-		if (hasAnimations)
+		if (path != '')
 		{
-			frames = Paths.getSparrowAtlas(path);
-			for (anim in animations)
+			if (hasAnimations)
 			{
-				animation.addByPrefix(anim.name, anim.prefixName, anim.frames, anim.looped, anim.flip[0], anim.flip[1]);
+				frames = Paths.getSparrowAtlas(path);
+				for (anim in animations)
+				{
+					animation.addByPrefix(anim.name, anim.prefixName, anim.frames, anim.looped, anim.flip[0], anim.flip[1]);
+				}
 			}
-		}
-		else
-		{
-			loadGraphic(path);
+			else
+			{
+				loadGraphic(path);
+			}
 		}
 		this.antialiasing = antialiasing;
 		scrollFactor.set(scrollX, scrollY);
 		this.active = active;
-
+	}
+	public function applyShader(shader:FlxShader)
+	{
+		this.shader = shader;
 	}
 }
