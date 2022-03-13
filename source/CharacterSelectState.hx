ede42f8d5b819cb4dedb26ac81d45709c97b6ff0
T5mpler <realt5mpler@gmail.com>
2022-03-12 21:58:06 -0500

    why is github fubg

--- source/CharacterSelectState.hx
+++ source/CharacterSelectState.hx
@@ -403,6 +403,7 @@ class CharacterSelectState extends MusicBeatState
 		if (!FlxG.save.data.charactersUnlocked.contains(character))
 		{
 			FlxG.save.data.charactersUnlocked.push(character);
+			trace('unlocked ' + character);
 		}
 	}
 	public static function isLocked(character:String):Bool
