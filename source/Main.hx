bbaf80325b9dfbaddbd6f2b715db49f97ed4ec94
T5mpler <realt5mpler@gmail.com>
2022-03-27 20:02:09 -0400

    fix keybinds for now

--- source/Main.hx
+++ source/Main.hx
@@ -1,79 +1,79 @@
-	package;
+package;
 
-	import flixel.FlxGame;
-	import flixel.FlxState;
-	import openfl.Assets;
-	import openfl.Lib;
-	import openfl.display.FPS;
-	import openfl.display.Sprite;
-	import openfl.events.Event;
-	import flixel.system.FlxSound;
-	import flixel.FlxG;
+import flixel.FlxGame;
+import flixel.FlxState;
+import openfl.Assets;
+import openfl.Lib;
+import openfl.display.FPS;
+import openfl.display.Sprite;
+import openfl.events.Event;
+import flixel.system.FlxSound;
+import flixel.FlxG;
 
-	class Main extends Sprite
+class Main extends Sprite
+{
+	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	var initialState:Class<FlxState> = StartStateSelector; // The FlxState the game starts with.
+	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
+	var framerate:Int = 144; // How many frames per second the game should run at.
+	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
+	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
+
+	static var fps:FpsDisplay;
+
+	public static var applicationName:String = "Friday Night Funkin' | VS. Dave and Bambi 3.0";
+
+	// You can pretty much ignore everything from here on - your code should go in your states.
+
+	public static function main():Void
 	{
-		var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
-		var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-		var initialState:Class<FlxState> = StartStateSelector; // The FlxState the game starts with.
-		var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-		var framerate:Int = 144; // How many frames per second the game should run at.
-		var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
-		var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
-		static var fps:FpsDisplay;
+		Lib.current.addChild(new Main());
+	}
 
-		public static var applicationName:String = "Friday Night Funkin' | VS. Dave and Bambi 3.0";
+	public function new()
+	{
+		super();
 
-		// You can pretty much ignore everything from here on - your code should go in your states.
+		if (stage != null)
+			init();
+		else
+			addEventListener(Event.ADDED_TO_STAGE, init);
+	}
 
-		public static function main():Void
+	private function init(?E:Event):Void
+	{
+		if (hasEventListener(Event.ADDED_TO_STAGE))
 		{
-			Lib.current.addChild(new Main());
+			removeEventListener(Event.ADDED_TO_STAGE, init);
 		}
 
-		public function new()
+		setupGame();
+	}
+
+	public static function toggleFuckedFPS(toggle:Bool)
+	{
+		fps.fuckFps = toggle;
+	}
+
+	private function setupGame():Void
+	{
+		var stageWidth:Int = Lib.current.stage.stageWidth;
+		var stageHeight:Int = Lib.current.stage.stageHeight;
+
+		if (zoom == -1)
 		{
-			super();
-
-			if (stage != null)
-				init();
-
-			else
-				addEventListener(Event.ADDED_TO_STAGE, init);
+			var ratioX:Float = stageWidth / gameWidth;
+			var ratioY:Float = stageHeight / gameHeight;
+			zoom = Math.min(ratioX, ratioY);
+			gameWidth = Math.ceil(stageWidth / zoom);
+			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
-		private function init(?E:Event):Void
-		{
-			if (hasEventListener(Event.ADDED_TO_STAGE))
-			{
-				removeEventListener(Event.ADDED_TO_STAGE, init);
-			}
+		initialState = StartStateSelector;
+		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-			setupGame();
-		}	
-
-		public static function toggleFuckedFPS(toggle:Bool)
-		{
-			fps.fuckFps = toggle;
-		}
-
-		private function setupGame():Void
-		{
-			var stageWidth:Int = Lib.current.stage.stageWidth;
-			var stageHeight:Int = Lib.current.stage.stageHeight;
-
-			if (zoom == -1)
-			{
-				var ratioX:Float = stageWidth / gameWidth;
-				var ratioY:Float = stageHeight / gameHeight;
-				zoom = Math.min(ratioX, ratioY);
-				gameWidth = Math.ceil(stageWidth / zoom);
-				gameHeight = Math.ceil(stageHeight / zoom);
-			}
-
-			initialState = StartStateSelector;
-			addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
-
-			var ourSource:String = "assets/videos/DO NOT DELETE OR GAME WILL CRASH/dontDelete.webm";
+		var ourSource:String = "assets/videos/DO NOT DELETE OR GAME WILL CRASH/dontDelete.webm";
 
 		#if web
 		var str1:String = "HTML CRAP";
@@ -85,7 +85,7 @@
 		GlobalVideo.setVid(vHandler);
 		vHandler.source(ourSource);
 		#elseif desktop
-		var str1:String = "WEBM SHIT"; 
+		var str1:String = "WEBM SHIT";
 		var webmHandle = new WebmHandler();
 		webmHandle.source(ourSource);
 		webmHandle.makePlayer();
@@ -94,9 +94,9 @@
 		GlobalVideo.setWebm(webmHandle);
 		#end
 
-			#if !mobile
-			fps = new FpsDisplay(10, 3, 0xFFFFFF);
-			addChild(fps);
-			#end
-		}
+		#if !mobile
+		fps = new FpsDisplay(10, 3, 0xFFFFFF);
+		addChild(fps);
+		#end
 	}
+}
