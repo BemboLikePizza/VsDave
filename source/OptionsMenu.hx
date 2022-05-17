365d3d7a30dba7615e7de0e7005c803b4a352af5
T5mpler <realt5mpler@gmail.com>
2022-05-17 01:59:04 -0400

    My langauge

--- source/OptionsMenu.hx
+++ source/OptionsMenu.hx
@@ -50,10 +50,9 @@ class OptionsMenu extends MusicBeatState
 			+ "\n" + (FlxG.save.data.eyesores ? LanguageManager.getTextString('option_eyesores_enabled') : LanguageManager.getTextString('option_eyesores_disabled')) 
 			+ "\n" + (FlxG.save.data.donoteclick ? LanguageManager.getTextString('option_selfAwareness_on') : LanguageManager.getTextString('option_selfAwareness_off'))
 			+ "\n" + (FlxG.save.data.donoteclick ? LanguageManager.getTextString('option_hitsound_on') : LanguageManager.getTextString('option_hitsound_off'))
-			+ "\n" + (LanguageManager.getTextString('option_fpsCap') + " " + FlxG.save.data.fpsCap)
 			+ "\n" + (FlxG.save.data.freeplayCuts ? LanguageManager.getTextString('option_freeplay_cutscenes_on') : LanguageManager.getTextString('option_freeplay_cutscenes_off'))
 			+ "\n" + (FlxG.save.data.noteCamera ? LanguageManager.getTextString('option_noteCamera_on') : LanguageManager.getTextString('option_noteCamera_off'))
-			+ "\n" + LanguageManager.getTextString('cur_language') + " " + LanguageManager.getTextString(LanguageManager.languageFromPathName(curLanguage).pathName)
+			+ "\n" + LanguageManager.getTextString('cur_language')
 			);
 		menuBG.color = 0xFFea71fd;
 		menuBG.setGraphicSize(Std.int(menuBG.width * 1.1));
@@ -88,93 +87,64 @@ class OptionsMenu extends MusicBeatState
 	{
 		super.update(elapsed);
 
-			if (controls.BACK)
-				FlxG.switchState(new MainMenuState());
-			if (controls.UP_P)
-				changeSelection(-1);
-			if (controls.DOWN_P)
-				changeSelection(1);
-			
-		if (curSelected == 7)
+		if (controls.BACK)
+			FlxG.switchState(new MainMenuState());
+		if (controls.UP_P)
+			changeSelection(-1);
+		if (controls.DOWN_P)
+			changeSelection(1);
+
+		if (controls.RIGHT_R)
 		{
-			if (controls.RIGHT_P)
+			FlxG.save.data.offset++;
+			versionShit.text = "Offset (Left, Right): " + FlxG.save.data.offset;
+		}
+
+		if (controls.LEFT_R)
+		{
+			FlxG.save.data.offset--;
+			versionShit.text = "Offset (Left, Right): " + FlxG.save.data.offset;
+		}	
+		if (controls.ACCEPT)
+		{
+			grpControls.remove(grpControls.members[curSelected]);
+			switch(curSelected)
 			{
-				var newValue = FlxG.save.data.fpsCap + 10;
-				FlxG.save.data.fpsCap = FlxMath.bound(newValue, 60, 300);
-				openfl.Lib.current.stage.frameRate = FlxG.save.data.fpsCap;
-				onFPSValueChanged();
-			}
-			if (controls.LEFT_P)
-			{
-				var newValue = FlxG.save.data.fpsCap - 10;
-				FlxG.save.data.fpsCap = FlxMath.bound(newValue, 60, 300);
-				openfl.Lib.current.stage.frameRate = FlxG.save.data.fpsCap;
-				onFPSValueChanged();
+				case 0:
+					new FlxTimer().start(0.01, function(timer:FlxTimer)
+					{
+						FlxG.switchState(new ChangeKeybinds());
+					});
+					updateGroupControls(LanguageManager.getTextString('change_keybind'), 0, 'Vertical');
+				case 1:
+					FlxG.save.data.newInput = !FlxG.save.data.newInput;
+					updateGroupControls((FlxG.save.data.newInput ? LanguageManager.getTextString('option_ghostTapping_on') : LanguageManager.getTextString('option_ghostTapping_off')), 1, 'Vertical');	
+				case 2:
+					FlxG.save.data.downscroll = !FlxG.save.data.downscroll;
+					updateGroupControls((FlxG.save.data.downscroll ? LanguageManager.getTextString('option_downscroll') : LanguageManager.getTextString('option_upscroll')), 2, 'Vertical');
+				case 3:
+					FlxG.save.data.songPosition = !FlxG.save.data.songPosition;
+					updateGroupControls((FlxG.save.data.songPosition ? LanguageManager.getTextString('option_songPosition_on') : LanguageManager.getTextString('option_songPosition_off')), 3, 'Vertical');	
+				case 4:
+					FlxG.save.data.eyesores = !FlxG.save.data.eyesores;
+					updateGroupControls((FlxG.save.data.eyesores ? LanguageManager.getTextString('option_eyesores_enabled') : LanguageManager.getTextString('option_eyesores_disabled')), 4, 'Vertical');
+				case 5:
+					FlxG.save.data.selfAwareness = !FlxG.save.data.selfAwareness;
+					updateGroupControls((FlxG.save.data.selfAwareness ? LanguageManager.getTextString('option_selfAwareness_on') : LanguageManager.getTextString('option_selfAwareness_off')), 5, 'Vertical');
+				case 6:
+					FlxG.save.data.donoteclick = !FlxG.save.data.donoteclick;
+					updateGroupControls((FlxG.save.data.donoteclick ? LanguageManager.getTextString('option_hitsound_on') : LanguageManager.getTextString('option_hitsound_off')), 6, 'Vertical');
+				case 7:
+					FlxG.save.data.freeplayCuts = !FlxG.save.data.freeplayCuts;
+					updateGroupControls((FlxG.save.data.freeplayCuts ? LanguageManager.getTextString('option_freeplay_cutscenes_on') : LanguageManager.getTextString('option_freeplay_cutscenes_off')), 7, 'Vertical');
+				case 8:
+					FlxG.save.data.noteCamera = !FlxG.save.data.noteCamera;
+					updateGroupControls((FlxG.save.data.noteCamera ? LanguageManager.getTextString('option_noteCamera_on') : LanguageManager.getTextString('option_noteCamera_off')), 8, 'Vertical');
+				case 9:
+					updateGroupControls(LanguageManager.getTextString('cur_language'), 9, 'Vertical');
+					FlxG.switchState(new ChangeLanguageState());
 			}
 		}
-		else
-		{
-			if (controls.RIGHT_R)
-			{
-				FlxG.save.data.offset++;
-				versionShit.text = "Offset (Left, Right): " + FlxG.save.data.offset;
-			}
-
-			if (controls.LEFT_R)
-			{
-				FlxG.save.data.offset--;
-				versionShit.text = "Offset (Left, Right): " + FlxG.save.data.offset;
-			}
-		}
-	
-
-			if (controls.ACCEPT)
-			{
-				grpControls.remove(grpControls.members[curSelected]);
-				switch(curSelected)
-				{
-					case 0:
-						new FlxTimer().start(0.01, function(timer:FlxTimer)
-						{
-							FlxG.switchState(new ChangeKeybinds());
-						});
-						updateGroupControls(LanguageManager.getTextString('change_keybind'), 0, 'Vertical');
-					case 1:
-						FlxG.save.data.newInput = !FlxG.save.data.newInput;
-						updateGroupControls((FlxG.save.data.newInput ? LanguageManager.getTextString('option_ghostTapping_on') : LanguageManager.getTextString('option_ghostTapping_off')), 1, 'Vertical');	
-					case 2:
-						FlxG.save.data.downscroll = !FlxG.save.data.downscroll;
-						updateGroupControls((FlxG.save.data.downscroll ? LanguageManager.getTextString('option_downscroll') : LanguageManager.getTextString('option_upscroll')), 2, 'Vertical');
-					case 3:
-						FlxG.save.data.songPosition = !FlxG.save.data.songPosition;
-						updateGroupControls((FlxG.save.data.songPosition ? LanguageManager.getTextString('option_songPosition_on') : LanguageManager.getTextString('option_songPosition_off')), 3, 'Vertical');	
-					case 4:
-						FlxG.save.data.eyesores = !FlxG.save.data.eyesores;
-						updateGroupControls((FlxG.save.data.eyesores ? LanguageManager.getTextString('option_eyesores_enabled') : LanguageManager.getTextString('option_eyesores_disabled')), 4, 'Vertical');
-					case 5:
-						FlxG.save.data.selfAwareness = !FlxG.save.data.selfAwareness;
-						updateGroupControls((FlxG.save.data.selfAwareness ? LanguageManager.getTextString('option_selfAwareness_on') : LanguageManager.getTextString('option_selfAwareness_off')), 5, 'Vertical');
-					case 6:
-						FlxG.save.data.donoteclick = !FlxG.save.data.donoteclick;
-						updateGroupControls((FlxG.save.data.donoteclick ? LanguageManager.getTextString('option_hitsound_on') : LanguageManager.getTextString('option_hitsound_off')), 6, 'Vertical');
-					case 8:
-						FlxG.save.data.freeplayCuts = !FlxG.save.data.freeplayCuts;
-						updateGroupControls((FlxG.save.data.freeplayCuts ? LanguageManager.getTextString('option_freeplay_cutscenes_on') : LanguageManager.getTextString('option_freeplay_cutscenes_off')), 8, 'Vertical');
-					case 9:
-						FlxG.save.data.noteCamera = !FlxG.save.data.noteCamera;
-						updateGroupControls((FlxG.save.data.noteCamera ? LanguageManager.getTextString('option_noteCamera_on') : LanguageManager.getTextString('option_noteCamera_off')), 9, 'Vertical');
-					case 10:
-						currentLanguage++;
-						if (currentLanguage > languages.length - 1)
-						{
-							currentLanguage = 0;
-						}
-						LanguageManager.save.data.language = languages[currentLanguage].pathName;
-						updateGroupControls(LanguageManager.getTextString('cur_language') + " " + LanguageManager.getTextString(LanguageManager.languageFromPathName(curLanguage).pathName), 10, 'Vertical');
-						LanguageManager.save.flush();
-						LanguageManager.init();
-				}
-			}
 	}
 
 	var isSettingControl:Bool = false;
@@ -227,10 +197,5 @@ class OptionsMenu extends MusicBeatState
 				// item.setGraphicSize(Std.int(item.width));
 			}
 		}
-	}
-	function onFPSValueChanged()
-	{
-		grpControls.remove(grpControls.members[curSelected]);
-		updateGroupControls(LanguageManager.getTextString('option_fpsCap') + " " + FlxG.save.data.fpsCap, 7, 'Vertical');
 	}
 }
