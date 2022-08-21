44ebbe3074bb4004809ea0425977bdbe3aa9f007
AM_Erizur <50849821+Erizur@users.noreply.github.com>
2022-08-21 16:55:34 -0500

    dead memes and unfunny jokes

--- source/CoolSystemStuff.hx
+++ source/CoolSystemStuff.hx
@@ -2,7 +2,6 @@ package;
 
 // crazy system shit!!!!! (only for windows)
 // lordryan wrote this :)
-#if windows
 import sys.io.File;
 import sys.io.Process;
 import haxe.io.Bytes;
@@ -12,24 +11,40 @@ class CoolSystemStuff
 	public static function getUsername():String
 	{
 		// uhh this one is self explanatory
+		#if windows
 		return Sys.getEnv("USERNAME");
+		#else
+		return Sys.getEnv("USER");
+		#end
 	}
 
 	public static function getUserPath():String
 	{
 		// this one is also self explantory
+		#if windows
 		return Sys.getEnv("USERPROFILE");
+		#else
+		return Sys.getEnv("HOME");
+		#end
 	}
 
 	public static function getTempPath():String
 	{
 		// gets appdata temp folder lol
+		#if windows
 		return Sys.getEnv("TEMP");
+		#else
+		// most non-windows os dont have a temp path, or if they do its not 100% compatible, so the user folder will be a fallback
+		return Sys.getEnv("HOME");
+		#end
 	}
 	public static function executableFileName()
 	{
+		#if windows
 		var programPath = Sys.programPath().split("\\");
+		#else
+		var programPath = Sys.programPath().split("/");
+		#end
 		return programPath[programPath.length - 1];
 	}
 }
-#end
