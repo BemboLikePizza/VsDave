e6b4b1271d4fb8a252891b0ec288463d276abfb5
rappareplol <85380065+rappareplol@users.noreply.github.com>
2022-08-20 20:53:49 -0400

    piss off

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -31,6 +31,7 @@ class GameOverSubstate extends MusicBeatSubstate
 				System.exit(0);
 			case 'dave' | 'dave-recursed':
 				daBf = 'dave-death';
+				deathSuffix = '-dave';
 			case 'tristan':
 				daBf = 'tristan-death';
 				deathSuffix = '-tristan';
@@ -42,6 +43,8 @@ class GameOverSubstate extends MusicBeatSubstate
 				deathSuffix = '-bambi';
 			case 'nofriend':
 				daBf = 'nofriend-death';
+				deathSuffix = '-nofriend';
+
 			default:
 				daBf = char;
 		}
@@ -65,7 +68,7 @@ class GameOverSubstate extends MusicBeatSubstate
 		camFollow = new FlxObject(bf.getGraphicMidpoint().x, bf.getGraphicMidpoint().y, 1, 1);
 		add(camFollow);
 
-		FlxG.sound.play(Paths.sound('fnf_loss_sfx' + deathSuffix));
+		FlxG.sound.play(Paths.sound('death/fnf_loss_sfx' + deathSuffix));
 		Conductor.changeBPM(100);
 
 		// FlxG.camera.followLerp = 1;
