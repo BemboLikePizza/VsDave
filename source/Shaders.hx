4dc19987c5e44f917ab1665763b4bb5eb6173f56
AM_Erizur <50849821+Erizur@users.noreply.github.com>
2022-08-28 12:30:36 -0500

    Revert "Revert "Revert "syntax error fixin for weird systems"""
    This reverts commit fe201253af208fc40bdc7f7f2457bb9656c8a83f.

--- source/Shaders.hx
+++ source/Shaders.hx
@@ -472,29 +472,26 @@ class DitherShader extends FlxShader
         float find_closest(int x, int y, float c0)
         {
 
-            int dither[8][8] = {
-            { 0, 32, 8, 40, 2, 34, 10, 42}, /* 8x8 Bayer ordered dithering */
-            {48, 16, 56, 24, 50, 18, 58, 26}, /* pattern. Each input pixel */
-            {12, 44, 4, 36, 14, 46, 6, 38}, /* is scaled to the 0..63 range */
-            {60, 28, 52, 20, 62, 30, 54, 22}, /* before looking in this table */
-            { 3, 35, 11, 43, 1, 33, 9, 41}, /* to determine the action. */
-            {51, 19, 59, 27, 49, 17, 57, 25},
-            {15, 47, 7, 39, 13, 45, 5, 37},
-            {63, 31, 55, 23, 61, 29, 53, 21} 
-            };
+        int dither[8][8] = {
+        { 0, 32, 8, 40, 2, 34, 10, 42}, /* 8x8 Bayer ordered dithering */
+        {48, 16, 56, 24, 50, 18, 58, 26}, /* pattern. Each input pixel */
+        {12, 44, 4, 36, 14, 46, 6, 38}, /* is scaled to the 0..63 range */
+        {60, 28, 52, 20, 62, 30, 54, 22}, /* before looking in this table */
+        { 3, 35, 11, 43, 1, 33, 9, 41}, /* to determine the action. */
+        {51, 19, 59, 27, 49, 17, 57, 25},
+        {15, 47, 7, 39, 13, 45, 5, 37},
+        {63, 31, 55, 23, 61, 29, 53, 21} };
 
-            float limit = 0.0;
-            if(x < 8)
-            {
-                limit = (dither[x][y]+1)/64.0;
-            }
+        float limit = 0.0;
+        if(x < 8)
+        {
+            limit = (dither[x][y]+1)/64.0;
+        }
 
 
-            if(c0 < limit)
-            {
-                return 0.0;
-                return 1.0;
-            }
+        if(c0 < limit)
+            return 0.0;
+            return 1.0;
         }
 
         void main(void)
