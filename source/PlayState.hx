6dfe8ca4e8315e08d2871479f72593ef3ba21d33
rappareplol <85380065+rappareplol@users.noreply.github.com>
2022-09-05 13:09:49 -0400

    Revert "fucked over ben"
    This reverts commit 7b09c5588aaa8fb1b1261aeff9a687046f0ec2b1.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1092,7 +1092,7 @@ class PlayState extends MusicBeatState
 		switch(SONG.song.toLowerCase())
 		{
 			case "exploitation":
-				funkyText = SONG.song + " - FUCKED";
+				funkyText = SONG.song; //who undid this.
 			case 'overdrive':
 				funkyText = '';
 			default:
